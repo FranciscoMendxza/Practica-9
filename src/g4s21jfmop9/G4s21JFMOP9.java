93eb8e46c02e2cd4ebc720284ac495f97f6d6e12
panch <panch@Mendxza>
2022-04-05 10:17:49 -0500

    Practica8

---
+++ src/g4s21jfmop9/G4s21JFMOP9.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package g4s21jfmop9;
+
+/**
+ *
+ * @author panch
+ */
+public class G4s21JFMOP9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        new FrmPrincipal().setVisible(true);
+        
+    }
+    
+}
